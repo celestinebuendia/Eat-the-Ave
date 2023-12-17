e50ff8ae804ad1e5e127d51402ba543c4c2d5a94
celestinebuendia <celestinembuendia@yahoo.com>
2023-12-17 05:03:31 -0800

    added sanity

--- eat-the-ave/app/components/navbar.js
+++ eat-the-ave/app/components/navbar.js
@@ -1,24 +1,30 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
-  return (
-    <nav className="border-b border-white sticky top-0 bg-black text-gray-100 z-10">
-    <div className="h-14 max-w-7xl p-4 mx-auto flex items-center justify-between">
-      <Link href="/" className="font-medium text-lg md:hover:underline">
-      Eat the Ave
-      </Link>
-      <ul className="flex items-center justify-end space-x-4 text-sm font-medium">
-      <li className="md:hover:underline">
-        <Link href="/eats">Eats</Link>
-      </li>
-      <li className="md:hover:underline">
-        <Link href="/history">History</Link>
-      </li>
-      <li className="md:hover:underline">
-        <Link href="/about">About</Link>
-      </li>
-      </ul>
-    </div>
-    </nav>
-  );
+  const pathname = usePathname();
+  if (pathname != "/studio") {
+    return (
+      <nav className="border-b border-white sticky top-0 bg-black text-gray-100 z-10">
+        <div className="h-14 max-w-7xl p-4 mx-auto flex items-center justify-between">
+          <Link href="/" className="font-medium text-lg md:hover:underline">
+            Eat the Ave
+          </Link>
+          <ul className="flex items-center justify-end space-x-4 text-sm font-medium">
+            <li className="md:hover:underline">
+              <Link href="/eats">Eats</Link>
+            </li>
+            <li className="md:hover:underline">
+              <Link href="/history">History</Link>
+            </li>
+            <li className="md:hover:underline">
+              <Link href="/about">About</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 }
