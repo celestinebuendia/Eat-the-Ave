ba86d5fbf85df310fdcf408fce2ea19e51d2b96e
celestinebuendia <celestinembuendia@yahoo.com>
2023-12-22 01:30:50 -0800

    Fixed shrinking icon bug

--- eat-the-ave/app/components/CheckBox.js
+++ eat-the-ave/app/components/CheckBox.js
@@ -2,5 +2,5 @@ import { MdOutlineCheckBox } from "react-icons/md";
 import { MdOutlineCheckBoxOutlineBlank } from "react-icons/md";
 
 export default function({checked}) {
-  return checked ? <MdOutlineCheckBox /> : <MdOutlineCheckBoxOutlineBlank />;
+  return checked ? <MdOutlineCheckBox className="flex-shrink-0"/> : <MdOutlineCheckBoxOutlineBlank className="flex-shrink-0"/>;
 }
