0c70efe4e45004b5453f36929b3991ba4b5d9f2f
celestinebuendia <celestinembuendia@yahoo.com>
2023-12-27 20:06:51 -0800

    Added navbar hover on smaller screens

--- eat-the-ave/app/components/Navbar.js
+++ eat-the-ave/app/components/Navbar.js
@@ -9,14 +9,14 @@ export default function Navbar() {
     return (
       <nav className="border-b border-white sticky top-0 bg-black text-gray-100 z-10">
         <div className="h-14 max-w-7xl p-4 mx-auto flex items-center justify-between">
-          <Link href="/" className="font-medium text-xl md:hover:text-primary-400">
+          <Link href="/" className="font-medium text-xl hover:text-primary-400">
             Home
           </Link>
           <ul className="flex items-center justify-end space-x-6 text-md font-medium">
-            <li className="md:hover:text-primary-400">
+            <li className="hover:text-primary-400">
               <Link href="/eateries">Eateries</Link>
             </li>
-            <li className="md:hover:text-primary-400">
+            <li className="hover:text-primary-400">
               <Link href="/eats">Eats</Link>
             </li>
             {/* <li className="md:hover:underline">
